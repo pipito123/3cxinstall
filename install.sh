23a4dbaee37c71ce43adbf68b4ae4e345c58de86
pipito123 <51008744+pipito123@users.noreply.github.com>
2019-05-24 23:16:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,12 @@ sudo apt-get install net-tools tshark mtr wireshark davfs2 cifs-utils net-tools 
 cd /home/omadmin/
 wget -o file https://github.com/febo01/3cxinstall/blob/master/Integration.zip
 
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 echo "Install 3CX PBX"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt update
 sudo apt install net-tools dphys-swapfile --yes
-sudo apt install 3cxpbx --yes
+sudo apt install 3cxpbx
