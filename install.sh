d32b661d41ad9bdc63ecc83569dcbf1538618d11
pipito123 <51008744+pipito123@users.noreply.github.com>
2019-05-25 01:01:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,9 @@ echo "INSTALL new apps"
 sudo apt-get install net-tools tshark mtr wireshark davfs2 cifs-utils net-tools dphys-swapfile -y
 
 cd /home/omadmin/
-wget -o file https://github.com/febo01/3cxinstall/blob/master/Integration.zip
-
+wget -o file https://github.com/pipito123/3cxinstall/blob/master/Integration.zip
 
 echo "Install 3CX PBX"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 apt-get update 
-sudo apt install net-tools dphys-swapfile 
-
