18e82746aa775a03e41e0cd03ac588b9d7cd8cb4
pipito123 <51008744+pipito123@users.noreply.github.com>
2019-05-25 00:20:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ sudo apt-get install net-tools tshark mtr wireshark davfs2 cifs-utils net-tools 
 
 cd /home/omadmin/
 wget -o file https://github.com/febo01/3cxinstall/blob/master/Integration.zip
-apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
+
 
 echo "Install 3CX PBX"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
-sudo apt update
+apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
 sudo apt install net-tools dphys-swapfile 
 sudo apt install 3cxpbx
