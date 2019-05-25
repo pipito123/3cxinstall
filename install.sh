9fa920ce83fc227e5a7e9e1591b4fee33a019314
pipito123 <51008744+pipito123@users.noreply.github.com>
2019-05-24 23:37:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,5 +13,5 @@ echo "Install 3CX PBX"
 wget -O- http://downloads-global.3cx.com/downloads/3cxpbx/public.key | sudo apt-key add -
 echo "deb http://downloads-global.3cx.com/downloads/debian stretch main" | sudo tee /etc/apt/sources.list.d/3cxpbx.list
 sudo apt update
-sudo apt install net-tools dphys-swapfile --yes
-sudo apt install 3cxpbx --force-yes
+sudo apt install net-tools dphys-swapfile 
+sudo apt install 3cxpbx -y
